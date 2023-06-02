7aa75b027ea5547807ec1b3291ef9a721d3a7c44
Lakindu Samarakoon <lakindusamarakoon@gmail.com>
2023-06-02 20:05:02 +0530

    Loggin error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,15 +6,25 @@ const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = {email: '', password: ''};
 
+    //incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'that email is not registerd';
+    }
+
+    //incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'that password is incorrect';
+    }
+
     //duplicate error code
     if (err.code === 11000){
-        errors.email = "This email is already registered";
+        errors.email = "that email is already registered";
         return errors;
     }
 
     //validation errors
     if (err.message.includes('user validation failed')) {
-        Object.values(err.errors).forEach(({properties})=> {
+        Object.values(err.errors).forEach(({ properties })=> {
             errors[properties.path] = properties.message;
         });
     }
@@ -26,7 +36,7 @@ const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) =>{
     return jwt.sign({ id }, 'laki secret', {
         expiresIn: maxAge
-    })
+    });
 }
 
 module.exports.signup_get = (req,res) => {
@@ -58,9 +68,13 @@ module.exports.login_post = async (req,res) => {
     
     try{
         const user = await User.login(email, password);
-        res.status(200).json({ user: user._id})
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+        res.status(200).json({ user: user._id});
+        
     }
     catch (err){
-        res.status(400).json({});
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 }
