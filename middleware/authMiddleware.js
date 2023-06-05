3e81ea2ddeef1a9fb272dfb19bc59b54752e2d2c
Lakindu Samarakoon <lakindusamarakoon@gmail.com>
2023-06-05 19:27:38 +0530

    Logout

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/Users')
 
 const requireAuth = ( req, res, next) => {
     const token = req.cookies.jwt;
@@ -9,18 +10,39 @@ const requireAuth = ( req, res, next) => {
             if(err){
                 console.log(err.message);
                 res.redirect('/login');
-
             }
             else{
                 console.log(decodedToken);
                 next();
             }
         });
-
     }
     else{
         res.redirect('/login');
     }
 }
 
-module.exports = { requireAuth };
+//check current user
+const checkUser = (req, res, next) => { 
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, 'laki secret', async (err, decodedToken) => {
+            if(err){
+                res.locals.user = null;
+                next();
+            }
+            else{
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next(); 
+            }
+        });
+    }
+    else{
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
